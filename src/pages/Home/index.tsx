825111bf07d36e1d4ff120f16d5646d5294500ad
Diego Almeida <dyhalmeida@gmail.com>
2020-06-05 09:28:50 -0300

    Added home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { FiLogIn } from "react-icons/fi";
+
+import "./style.css";
+import Logo from "../../assets/logo.svg";
+
+const Home = () => {
+  return (
+    <div id="page-home">
+      <div className="content">
+        <header>
+          <img src={Logo} alt="Ecoleta" />
+        </header>
+        <main>
+          <h1>Seu marketplace de coleta de resíduos</h1>
+          <p>
+            Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
+          </p>
+          <a href="/">
+            <span>
+              <FiLogIn />
+            </span>
+            <strong>Cadastre um ponto de coleta</strong>
+          </a>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
