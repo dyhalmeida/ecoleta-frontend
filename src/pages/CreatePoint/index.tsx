bb61fb3ca03274e6fc370568744a79b4ffff776b
Diego Almeida <dyhalmeida@gmail.com>
2020-06-07 11:09:50 -0300

    added dropzone upload image

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -12,6 +12,7 @@ import cityService from "../../services/city";
 
 import IItem from "../../models/item";
 import Item from "../../components/Item";
+import Dropzone from "../../components/Dropzone";
 
 import "./style.css";
 import Logo from "../../assets/logo.svg";
@@ -32,6 +33,7 @@ const CreatePoint: React.FC = () => {
     whatsapp: "",
   });
 
+  const [selectedFile, setSelectedFile] = useState<File>();
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
   const [selectedUf, setSelectedUf] = useState("0");
   const [selectedCity, setSelectedCity] = useState("0");
@@ -129,17 +131,20 @@ const CreatePoint: React.FC = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      image: "fake-image",
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items,
-    };
+    const data = new FormData();
+
+    data.append("name", name);
+    data.append("email", email);
+    data.append("whatsapp", whatsapp);
+    data.append("uf", uf);
+    data.append("city", city);
+    data.append("latitude", String(latitude));
+    data.append("longitude", String(longitude));
+    data.append("items", items.join(","));
+
+    if (selectedFile) {
+      data.append("image", selectedFile);
+    }
 
     collectService(data).subscribe(() => history.push("/"));
   };
@@ -157,6 +162,7 @@ const CreatePoint: React.FC = () => {
         <h1>
           Cadastro do <br /> ponto de coleta
         </h1>
+        <Dropzone onFileUploaded={setSelectedFile}></Dropzone>
         <fieldset>
           <legend>
             <h2>Dados</h2>
